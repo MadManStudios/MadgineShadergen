a34fadcadeb166f02bf9a265468c3786c9200d93
Martin Schütz <madmanrises@web.de>
2026-01-02 00:01:43 +0100

    Generating code for constant buffers

--- generateCPP.cpp
+++ generateCPP.cpp
@@ -18,7 +18,11 @@
 
 #include <map>
 
+#include <set>
+
 #include <optional>
+
+#include <sstream>
 
 #include "dxc/Support/D3DReflection.h"
 
@@ -35,6 +39,13 @@ struct FunctionSignature {
 };
 
 #define check(c) if (*s++ != c) {--s; return {};}
+
+void dummy(std::ostream& out, size_t count = 1) {
+	static size_t i = 0;
+	for (size_t j = 0; j < count; ++j) {
+		out << "float _dummy" << i++ << ";\n";
+	}
+}
 
 std::string demangleType(const char*& s, std::vector<std::string>& types, std::vector<std::string>& names);
 
@@ -204,6 +215,148 @@ std::string patchType(std::string s) {
 	return s;
 }
 
+int generateStruct(ID3D12ShaderReflectionType* type, UINT& size, std::ostream& of, std::map<std::string, UINT>& generatedStructs);
+
+int writeType(ID3D12ShaderReflectionType* type, const char* name, UINT& size, std::ostream& target, std::ostream& of, std::map<std::string, UINT>& generatedStructs) {
+
+	D3D12_SHADER_TYPE_DESC typeDesc;
+	HRESULT hr = type->GetDesc(&typeDesc);
+	CHECK_HR(ConstantBuffer / Type / GetDesc);
+
+	std::string typeName = typeDesc.Name;
+
+	switch (typeDesc.Class) {
+	case D3D_SVC_SCALAR:
+		size = 4;
+		if (typeDesc.Type == D3D_SVT_BOOL)
+			typeName = "uint32_t";
+		break;
+	case D3D_SVC_STRUCT:
+		if (int result = generateStruct(type, size, of, generatedStructs))
+			return result;
+		break;
+	case D3D_SVC_VECTOR:
+		typeName = "Engine::Vector" + std::to_string(typeDesc.Columns);
+		if (typeDesc.Type == D3D_SVT_INT)
+			typeName += "i";
+		size = 4 * typeDesc.Columns;
+		break;
+	case D3D_SVC_MATRIX_ROWS:
+	case D3D_SVC_MATRIX_COLUMNS:
+		assert(typeDesc.Columns == typeDesc.Rows);
+		typeName = "Engine::Matrix" + std::to_string(typeDesc.Columns);
+		size = 4 * typeDesc.Columns * typeDesc.Rows;
+		break;
+	default:
+		std::cerr << "Unsupported type class: " << typeDesc.Class << std::endl;
+		return -1;
+	}
+
+
+	target << "    " << typeName << " " << name;
+
+	if (typeDesc.Elements > 0) {
+		target << "[" + std::to_string(typeDesc.Elements) + "]";
+		size += (((size - 1) / 16) + 1) * 16 * (typeDesc.Elements - 1);
+	}
+
+	target << ";";
+
+	return 0;
+}
+
+int generateStruct(ID3D12ShaderReflectionType* type, UINT& size, std::ostream& of, std::map<std::string, UINT>& generatedStructs) {
+	D3D12_SHADER_TYPE_DESC desc;
+	HRESULT hr = type->GetDesc(&desc);
+	CHECK_HR(Type / GetDesc);
+
+	auto pib = generatedStructs.try_emplace(desc.Name);
+	if (pib.second) {
+
+		std::stringstream ss;
+
+		ss << "struct " << desc.Name << "{\n";
+
+		size = 0;
+
+		for (UINT i = 0; i < desc.Members; ++i) {
+			ID3D12ShaderReflectionType* memberType = type->GetMemberTypeByIndex(i);
+
+			D3D12_SHADER_TYPE_DESC memberDesc;
+			hr = memberType->GetDesc(&memberDesc);
+			CHECK_HR(Type / Member / GetDesc);
+
+			dummy(ss, (memberDesc.Offset - size) / 4);
+
+			UINT memberSize = 0;
+
+			if (int result = writeType(memberType, type->GetMemberTypeName(i), memberSize, ss, of, generatedStructs))
+				return result;
+
+			ss << " // offset: " << memberDesc.Offset << ", size: " << memberSize << "\n";
+
+			size = memberDesc.Offset + memberSize;
+		}
+
+		ss << "}; // size: " << size;
+
+		of << ss.str() << "\n\n";
+
+		pib.first->second = size;
+	}
+
+	size = pib.first->second;
+
+	return 0;
+}
+
+int generateConstantBufferStruct(ID3D12ShaderReflectionConstantBuffer* cb, std::ostream& of, std::map<std::string, UINT>& generatedStructs) {
+	D3D12_SHADER_BUFFER_DESC desc;
+	HRESULT hr = cb->GetDesc(&desc);
+	CHECK_HR(ConstantBuffer / GetDesc);
+
+	auto pib = generatedStructs.try_emplace(desc.Name);
+	if (pib.second) {
+
+		std::stringstream ss;
+
+		ss << "struct " << desc.Name << "{\n";
+
+		UINT currentSize = 0;
+
+		for (UINT i = 0; i < desc.Variables; ++i) {
+			ID3D12ShaderReflectionVariable* variable = cb->GetVariableByIndex(i);
+			D3D12_SHADER_VARIABLE_DESC varDesc;
+			hr = variable->GetDesc(&varDesc);
+			CHECK_HR(ConstantBuffer / Variable / GetDesc);
+
+			dummy(ss, (varDesc.StartOffset - currentSize) / 4);
+
+			UINT size = 0;
+
+			if (int result = writeType(variable->GetType(), varDesc.Name, size, ss, of, generatedStructs))
+				return result;
+
+			ss << "// offset: " << varDesc.StartOffset << ", size: " << size << "\n";
+
+			if (size != varDesc.Size) {
+				std::cerr << "Size mismatch: " << desc.Name << "::" << varDesc.Name << " " << size << " - " << varDesc.Size << std::endl;
+				//return -1;
+			}
+
+			currentSize = varDesc.StartOffset + varDesc.Size;
+		}
+
+		ss << "}; // size: " << currentSize;
+
+		of << ss.str() << "\n\n";
+
+		pib.first->second = currentSize;
+	}
+
+	return 0;
+}
+
 int generateCPP(const std::wstring& _filePath, const std::wstring& outFolder, const DxcBuffer& sourceBuffer, std::vector<LPCWSTR> arguments, const std::vector<std::wstring>& includes, std::map<std::wstring, std::vector<std::wstring>>& profileEntrypoints) {
 
 	std::cout << "CPP ... ";
@@ -225,7 +378,7 @@ int generateCPP(const std::wstring& _filePath, const std::wstring& outFolder, co
 	CHECK_HR(GetOutput / Errors);
 	if (pErrors && pErrors->GetStringLength() > 0) {
 		std::cerr << "Error compiling HLSL" << std::endl;
-		std::cerr << (char*)pErrors->GetBufferPointer() << std::endl;		
+		std::cerr << (char*)pErrors->GetBufferPointer() << std::endl;
 	}
 
 	if (FAILED(compileError)) {
@@ -270,19 +423,21 @@ int generateCPP(const std::wstring& _filePath, const std::wstring& outFolder, co
 
 		of << "namespace HLSL {\n";
 
-		of << "    inline const Engine::Render::ShaderMetadata file_" << baseName << "{\n\
-        \"" << filePath << "\",\n\
-        {\n";
+		of << "inline const Engine::Render::ShaderMetadata file_" << baseName << "{\n\
+    \"" << filePath << "\",\n\
+    {\n";
 		bool first = true;
 		for (const std::wstring& include : includes) {
 			if (first)
 				first = false;
 			else
 				of << ", \n";
-			of << "            \"" << converter.to_bytes(include) << "\"";
+			of << "        \"" << converter.to_bytes(include) << "\"";
 		}
-		of << "\n        }\n\
-    };\n\n";
+		of << "\n    }\n\
+};\n\n";
+
+		std::map<std::string, UINT> generatedStructs;
 
 		for (UINT i = 0; i < desc.FunctionCount; ++i) {
 			ID3D12FunctionReflection* function = reflection->GetFunctionByIndex(i);
@@ -292,6 +447,32 @@ int generateCPP(const std::wstring& _filePath, const std::wstring& outFolder, co
 
 			const char* s = functionDesc.Name;
 			std::optional<FunctionSignature> signature = demangle(s);
+
+			std::vector<std::string> constantBufferBindings;
+
+			for (UINT j = 0; j < functionDesc.ConstantBuffers; ++j) {
+				ID3D12ShaderReflectionConstantBuffer* cb = function->GetConstantBufferByIndex(j);
+				if (int result = generateConstantBufferStruct(cb, of, generatedStructs))
+					return result;
+
+				D3D12_SHADER_BUFFER_DESC desc;
+				hr = cb->GetDesc(&desc);
+				CHECK_HR(Reflect / ConstantBuffer / GetDesc);
+
+				D3D12_SHADER_INPUT_BIND_DESC bindDesc;
+				hr = function->GetResourceBindingDescByName(desc.Name, &bindDesc);
+				CHECK_HR(Reflect / ConstantBuffer / GetBinding);
+
+				if (bindDesc.Space == 0) {
+					if (constantBufferBindings.size() <= bindDesc.BindPoint)
+						constantBufferBindings.resize(bindDesc.BindPoint + 1);
+					if (!constantBufferBindings[bindDesc.BindPoint].empty() && constantBufferBindings[bindDesc.BindPoint] != desc.Name) {
+						std::cerr << "Bind Point " << bindDesc.BindPoint << " used for " << desc.Name << " and " << constantBufferBindings[bindDesc.BindPoint] << std::endl;
+						return -1;
+					}
+					constantBufferBindings[bindDesc.BindPoint] = desc.Name;
+				}
+			}
 
 			std::string suffix = signature->name.substr(signature->name.size() - 2);
 			if (suffix == "VS") {
@@ -311,10 +492,18 @@ int generateCPP(const std::wstring& _filePath, const std::wstring& outFolder, co
 				forwardDeclare(of, param);
 			}
 
-			of << "    inline Engine::Render::ShaderFileObject<" << patchType(signature->returnType);
+			of << "inline Engine::Render::ShaderFileObject<" << patchType(signature->returnType);
 
 			for (std::string param : signature->parameterTypes) {
 				of << ", " << patchType(param);
+			}
+			for (std::string binding : constantBufferBindings) {
+				if (binding.empty()) {
+					of << ", void";
+				}
+				else {
+					of << ", " << binding;
+				}
 			}
 			of << "> " << signature->name << " { file_" << baseName << ", \"" << signature->name << "\", {} };\n";
 		}
